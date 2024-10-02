46261f47bae4aee65d121749be46bf75ffeace24
Vadim T <1hananam1ty@gmail.com>
2024-10-02 14:41:52 +0300

    reset fields on submit

--- src/app/CodeWarsModule/CodeWarsAddForm.tsx
+++ src/app/CodeWarsModule/CodeWarsAddForm.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 import { cn } from '@bem-react/classname';
 import { Button, Stack, TextareaAutosize, TextField } from '@mui/material';
 import { useForm } from 'react-hook-form';
@@ -10,12 +10,21 @@ const cnCodeWarsAddForm = cn('CodeWarsAddForm');
 
 interface CodeWarsAddFormProps {
   className?: string;
+  onReset?: () => void;
 }
 
 export const CodeWarsAddForm: FC<CodeWarsAddFormProps> = (props) => {
   const codeWarsRef = collection(db, 'codeWars');
 
-  const { register, handleSubmit } = useForm<CodeWarsUnitModel>();
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState,
+    formState: { isSubmitSuccessful },
+  } = useForm<CodeWarsUnitModel>({
+    defaultValues: { unitId: '', description: '', solution: '', hashtag: '' },
+  });
 
   const onSubmit = handleSubmit(async (data): Promise<void> => {
     await setDoc(doc(codeWarsRef, data.unitId), {
@@ -26,10 +35,17 @@ export const CodeWarsAddForm: FC<CodeWarsAddFormProps> = (props) => {
     });
   });
 
+  useEffect(() => {
+    if (isSubmitSuccessful) {
+      reset();
+    }
+  }, [formState, isSubmitSuccessful, reset]);
+
   return (
     <form
       className={cnCodeWarsAddForm(undefined, [props.className])}
       onSubmit={onSubmit}
+      onReset={props.onReset}
     >
       <Stack gap={1}>
         <TextField {...register('unitId')} variant={'outlined'} label={'№'} />
