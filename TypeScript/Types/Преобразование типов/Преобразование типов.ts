c3ae74be704fd0ae709a0112db3d0921df4f7f8a
Vadim T <1hananam1ty@gmail.com>
2025-07-04 16:10:13 +0300

    pew

--- TypeScript/Types/Преобразование типов/Преобразование типов.ts
+++ TypeScript/Types/Преобразование типов/Преобразование типов.ts
@@ -34,3 +34,42 @@ const obj3 = {
   username: 'Aboba',
   password: '123456',
 } satisfies Person;
+
+//
+const PersonKeys = {
+  age: '30',
+  username: 'Abobus',
+  password: '123456',
+};
+
+function keys<T extends object>(data: T): Array<keyof T> {
+  return Object.keys(data) as Array<keyof T>;
+}
+
+const k = keys(PersonKeys);
+
+//
+
+type EmptyObject = {};
+
+const obj4: EmptyObject = { age: '123' };
+const obj5: EmptyObject = 1;
+const obj6: EmptyObject = '';
+const obj7: EmptyObject = () => {};
+const obj8: EmptyObject = null;
+const obj9: EmptyObject = undefined;
+const obj10: Object = {};
+const obj11: Object = 1;
+const obj12: Object = '';
+const obj13: Object = () => {};
+const obj14: Object = null;
+const obj15: Object = undefined;
+
+const obj16: object = {};
+const obj17: object = () => {};
+const obj18: object = new Date();
+
+const obj19: object = 1;
+const obj20: object = '';
+const obj21: object = null;
+const obj22: object = undefined;
